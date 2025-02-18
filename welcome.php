df3447fa07d224cf84154a88ec2e2d710cfb1b91
sriramp16 <143397656+sriramp16@users.noreply.github.com>
2025-02-18 08:18:37 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <style>
+        .welcome-page{
+            text-align: center;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="welcome-page">
+        <h1>Welcome to Login System in php using xampp.</h1>
+        <a href="login.php">Click to Login</a>
+        <br/>
+        <br/>
+        <a href="logout.php">Click to LOGOUT</a>
+    </div>
+</body>
+</html>
